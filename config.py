ea6dd9880461912d1e5b36019f4626cccc7ea050
KevinFett1985 <105512566+KevinFett1985@users.noreply.github.com>
2024-12-01 18:00:04 -0600

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG=True
-    MYSQL_HOST='localhost'
-    MYSQL_USER='root'
-    MYSQL_PASSWORD='root'
+    MYSQL_HOST='swalbd.c9soamye8quw.us-east-1.rds.amazonaws.com'
+    MYSQL_USER='admin'
+    MYSQL_PASSWORD='Adminroot'
     MYSQL_DB='swal_bd2'
     MAIL_SERVER='smtp.googlemail.com'
     MAIL_PORT=587
